3b2024316c077191f9c8eb0f853d81b50bb0c554
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 11:05:18 +0500

    feat(widgets/student-profile): create StudentProfileOverview skeleton

---
+++ src/widgets/student-profile/StudentProfileOverview.tsx
@@ -0,0 +1,7 @@
+﻿export const StudentProfileOverview = async () => {
+  return (
+    <div className="border border-gray-200 p-6 flex flex-col gap-4">
+      <p className="text-gray-500">Loading profile overview...</p>
+    </div>
+  );
+};
