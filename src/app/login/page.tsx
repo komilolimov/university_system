96f61e77280b98d6c025fff7905a67e0eee3fa3f
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 10:50:36 +0500

    feat(app/login): create login page skeleton

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+﻿export default function LoginPage() {
+  return (
+    <main className="fsd-container mx-auto my-16 max-w-md flex flex-col gap-8">
+      <div>Content here</div>
+    </main>
+  );
+}
