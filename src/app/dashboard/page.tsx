c0a9fddc79b0eb1f0b6d0a6798b53b87a46954ea
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 10:56:31 +0500

    feat(app/dashboard): integrate LogoutButton into dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
-﻿export default function DashboardPage() {
+﻿import { LogoutButton } from "@/features/auth/logout";
+
+export default function DashboardPage() {
   return (
     <main className="fsd-container mx-auto my-16 max-w-5xl flex flex-col gap-8">
+      <div className="flex justify-end">
+        <LogoutButton />
+      </div>
       <div>Dashboard Content</div>
     </main>
   );
