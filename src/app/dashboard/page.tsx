7eb83724b349c13167675c98b5730eff6fddfa99
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 10:56:31 +0500

    feat(app/dashboard): create dashboard page skeleton

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+﻿export default function DashboardPage() {
+  return (
+    <main className="fsd-container mx-auto my-16 max-w-5xl flex flex-col gap-8">
+      <div>Dashboard Content</div>
+    </main>
+  );
+}
