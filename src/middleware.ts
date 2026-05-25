1fc6a5d9b540b4195b55bd1c8cc7aa276f651c20
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 11:19:09 +0500

    refactor(middleware): clear old middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,6 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("access_token")?.value;
-
-  if (request.nextUrl.pathname.startsWith("/dashboard") && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
   return NextResponse.next();
 }
 
