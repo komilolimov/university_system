0ac729a6fe44c58673609c137810ca9b2291a5cd
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 10:41:53 +0500

    feat(app/courses): create courses page component

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,7 @@
+﻿export default function CoursesPage() {
+  return (
+    <main className="fsd-container mx-auto my-8 max-w-4xl flex flex-col gap-8">
+      <div>Content here</div>
+    </main>
+  );
+}
