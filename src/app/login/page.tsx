5e2732916397a733c989f51212eef7ae069f3cb4
Komil Olimov <k.olimov@uzinfocom.uz>
2026-05-25 10:50:36 +0500

    feat(app/login): integrate LoginForm into page layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
-﻿export default function LoginPage() {
+﻿import { LoginForm } from "@/features/auth/login-by-email";
+
+export default function LoginPage() {
   return (
     <main className="fsd-container mx-auto my-16 max-w-md flex flex-col gap-8">
-      <div>Content here</div>
+      <header className="flex flex-col gap-2 border-b border-gray-300 pb-4 text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Sign In</h1>
+        <p className="text-gray-500 font-medium">Access the University System</p>
+      </header>
+      <section>
+        <LoginForm />
+      </section>
     </main>
   );
 }
